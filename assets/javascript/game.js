466b8694b63d4b0e8edff171398b4c53f43f4d93
Jeremy Parra <jeremydeanparra@gmail.com>
2018-11-12 22:01:09 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,39 @@
-// Get four buttons on screen, onclick the buttons will add associated value to player total score
-// Assign a random value between 1-12 to each button every time the game is reset
-// Create an input box for random gennerated “winning number” 19-120
-// Generate “winning number”
-// Create player score display and value
-// Game reset when win or lose condition met
 
-// Global Variables
-// ********************************************************************************
 
-var playerScore = 0;
-var gameOn = true;
 
 // Functions
 // ********************************************************************************
 
 $(document).ready(function () {
 
+    var playerScore = 0;
     var computerNum = Math.floor(Math.random() * (120 - 19)) + 19;
     var yellowCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
     var redCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
     var greenCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
     var purpleCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
 
+    function blinker() {
+        $('.blinking').fadeOut(1000);
+        $('.blinking').fadeIn(1000);
+    }
+    setInterval(blinker, 1000);
+
+    function winner() {
+        $("#playerNum").css("color", "green");
+        $("#playerMessage").css("color", "green");
+        $("#playerMessage").text("YOU WIN!!! CLICK HERE TO RESET");
+        $(".myButton").prop('disabled', true);
+        blinker();
+    }
+
+    function loser() {
+        $("#playerNum").css("color", "red");
+        $("#playerMessage").css("color", "red");
+        $("#playerMessage").text("YOU LOSE!!! CLICK HERE TO RESET");
+        $(".myButton").prop('disabled', true);
+        blinker();
+    }
 
     function reset() {
         computerNum = Math.floor(Math.random() * (120 - 19)) + 19;
@@ -32,31 +44,30 @@ $(document).ready(function () {
         playerScore = 0;
         $("#compNum").text(computerNum);
         $("#playerNum").text(playerScore);
-        
+        $("#playerMessage").text("");
+        $("#playerNum").css("color", "black");
+        $("#playerMessage").css("color", "red");
+        $('.myButton').prop('disabled', false);
+
         console.log("Winning Number is: " + computerNum);
         console.log("Yellow Crystal = " + yellowCrystal);
         console.log("Red Crystal = " + redCrystal);
         console.log("Green Crystal = " + greenCrystal);
         console.log("Purple Crystal = " + purpleCrystal);
         console.log("------------------------------------");
-
     }
 
-    reset();
-    
 
+    reset();
 
     $("#yellowButton").on("click", function () {
         playerScore = playerScore + yellowCrystal;
         $("#playerNum").text(playerScore);
 
         if (playerScore == computerNum) {
-            $("#playerNum").css("color", "green");
-            $("#playerMessage").text("YOU WIN!!! CLICK HERE TO RESET");
+            winner();
         } else if (playerScore > computerNum) {
-            $("#playerNum").css("color", "red");
-            $("#playerMessage").text("YOU LOSE!!!");
-            reset();
+            loser();
         }
     });
 
@@ -65,13 +76,9 @@ $(document).ready(function () {
         $("#playerNum").text(playerScore);
 
         if (playerScore == computerNum) {
-            $("#playerNum").css("color", "green");
-            $("#playerMessage").text("YOU WIN!!! CLICK HERE TO RESET");
+            winner();
         } else if (playerScore > computerNum) {
-            $("#playerNum").css("color", "red");
-            $("#playerMessage").text("YOU LOSE!!!");
-            reset();
-
+            loser();
         }
     });
 
@@ -80,13 +87,9 @@ $(document).ready(function () {
         $("#playerNum").text(playerScore);
 
         if (playerScore == computerNum) {
-            $("#playerNum").css("color", "green");
-            $("#playerMessage").text("YOU WIN!!! CLICK HERE TO RESET");
+            winner();
         } else if (playerScore > computerNum) {
-            $("#playerNum").css("color", "red");
-            $("#playerMessage").text("YOU LOSE!!!");
-            reset();
-
+            loser();
         }
     });
 
@@ -95,14 +98,17 @@ $(document).ready(function () {
         $("#playerNum").text(playerScore);
 
         if (playerScore == computerNum) {
-            $("#playerNum").css("color", "green");
-            $("#playerMessage").text("YOU WIN!!! CLICK HERE TO RESET");
-
+            winner();
         } else if (playerScore > computerNum) {
-            $("#playerNum").css("color", "red");
-            $("#playerMessage").text("YOU LOSE!!!");
-            reset();
+            loser();
+        }
+    });
 
+    $("#playerMessage").on("click", function () {
+        if (playerScore != computerNum && playerScore < computerNum) {
+            return;
+        } else {
+            reset();
         }
     });
 });
